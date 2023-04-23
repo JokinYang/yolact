feb0c3222be04e5db2e155ededd5ec71c369b534
Jokin Yang <Jokin@5p2o.com>
2023-04-23 09:34:01 +0800

    change backbone to resnet10t and some train params

--- nets/yolact.py
+++ nets/yolact.py
@@ -126,7 +126,7 @@ class Yolact(nn.Module):
         #   获得的C5为17, 17, 2048
         #----------------------------#
         import timm
-        self.backbone = timm.create_model("mobilenetv3_small_100", pretrained=pretrained,features_only=True,out_indices=[2,3,4])
+        self.backbone = timm.create_model("resnet10t", pretrained=pretrained,features_only=True,out_indices=[2,3,4])
         # self.backbone               = ResNet(layers=[3, 4, 6, 3])
         # if pretrained:
         #     self.backbone.load_state_dict(torch.load("model_data/resnet50_backbone_weights.pth"))
@@ -138,7 +138,7 @@ class Yolact(nn.Module):
         #   获得的P6为9, 9, 256
         #   获得的P7为5, 5, 256
         #----------------------------#
-        self.fpn                    = FPN([24, 48, 576])
+        self.fpn                    = FPN([128, 256, 512])
         
         #--------------------------------#
         #   对P3进行上采样
